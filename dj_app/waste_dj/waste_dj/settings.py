5a92a79634c3ce81ebf576d086450c070aa8c815
Bogdan Popesuc <bogdn.ctin18@gmail.com>
2022-07-02 13:37:37 +0300

    blog_app

--- dj_app/waste_dj/waste_dj/settings.py
+++ dj_app/waste_dj/waste_dj/settings.py
@@ -39,7 +39,7 @@ if DEBUG:
 INSTALLED_APPS = [
     'personal',
     'account',
-
+    'blog',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,7 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- dj_app/waste_dj/waste_dj/urls.py
+++ dj_app/waste_dj/waste_dj/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from personal.views import home_screen_view
 from account.views import (registration_view, logout_view, login_view, account_view)
@@ -46,3 +48,7 @@ urlpatterns = [
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
