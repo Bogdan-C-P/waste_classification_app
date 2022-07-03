881802f51dad2122142517a3cdf81bd7c683b5a7
Bogdan Popesuc <bogdn.ctin18@gmail.com>
2022-07-03 22:38:04 +0300

    fixed buttons

--- dj_app/waste_dj/account/views.py
+++ dj_app/waste_dj/account/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login,authenticate,logout
 
 from .forms import RegistrationForm,AccountAuthenticationForm,AccountUpdateForm
+from blog.models import BlogPost
+
 # Create your views here.
 
 def registration_view(request):
@@ -74,4 +76,9 @@ def account_view(request):
             }
         )
     context['account_form'] = form
+    blog_posts = BlogPost.objects.filter(author=request.user)
+    context['blog_posts'] = blog_posts
     return render(request,'account/account.html',context)
+
+def must_authenticate_view(request):
+	return render(request, 'account/must_authenticate.html', {})
